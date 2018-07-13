455d14c818fed18b395fe9c732efdb401d7afbfe
Kaitlyn Hova <kaitlynhova@gmail.com>
2018-07-13 12:24:20 -0700

    adding card to app

--- supportpost/src/App.js
+++ supportpost/src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./Card/Card";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        {
+          id: "aaaah",
+          createdAt: Date.now(),
+          reason: "Because it's the right thing to do",
+          email: "Hova@hovalabs.com",
+          location: "Omaha, NE"
+        }
+      ]
+    };
+  }
   render() {
+    var cardList = this.state.cards.map((card, i) => {
+      return <Card card={card} key={i} />;
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>SupportCards</h1>
+        {cardList}
       </div>
     );
   }
---
+++ supportpost/src/Card/Card.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./Card.css";
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.card = props.card;
+  }
+  render(props) {
+    return (
+      <div>
+        <p>{this.card.reason}</p>
+      </div>
+    );
+  }
+}
+
+Card.propTypes = PropTypes.shape({
+  id: PropTypes.string,
+  createdAt: PropTypes.instanceOf(Date),
+  reason: PropTypes.string,
+  email: PropTypes.string,
+  location: PropTypes.string
+});
+
+export default Card;
