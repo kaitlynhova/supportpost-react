f5cd9a4fd52861a729d063726f87f5ff01a60cda
Kaitlyn Hova <kaitlynhova@gmail.com>
2018-07-14 12:45:12 -0700

    Adding home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Campaigns from "./Campaigns/Campaigns";
 import Cards from "./Cards/Cards";
+import Home from "./Home";
 
 import "./App.css";
 
@@ -10,14 +11,18 @@ const Navigation = () => (
     <div>
       <ul>
         <li>
-          <a href="/">Campaigns</a>
+          <a href="/">Home</a>
         </li>
         <li>
           <a href="/cards">Cards</a>
         </li>
+        <li>
+          <a href="/campaigns">Campaigns</a>
+        </li>
       </ul>
-      <Route exact path="/" component={Campaigns} />
+      <Route exact path="/" component={Home} />
       <Route path="/cards" component={Cards} />
+      <Route path="/campaigns" component={Campaigns} />
     </div>
   </Router>
 );
