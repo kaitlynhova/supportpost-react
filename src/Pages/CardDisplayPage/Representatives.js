8d9a40963fe94251c600d94640df0847b4d7dc6b
kaitlynhova <kaitlynhova@gmail.com>
2018-09-15 09:19:26 -0700

    Adding Axios and Propublica data to Representatives.js

--- src/Pages/CardDisplayPage/Representatives.js
+++ src/Pages/CardDisplayPage/Representatives.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import propublica from '../../Images/Propublica.svg';
 import { Container } from '../../DesignSystem/Container';
@@ -7,8 +8,29 @@ class Representatives extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: props.location
+      location: props.location,
+      house: [],
+      senate: []
     };
+    this.getRepresentatives('house');
+    this.getRepresentatives('senate');
+  }
+
+  getRepresentatives(chamber) {
+    axios
+      .get(`https://api.propublica.org/congress/v1/members/${chamber}/${this.props.location}/current.json`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: { 'X-API-Key': process.env.REACT_APP_PROPUBLICA_X_API_KEY }
+      })
+      .then((response) => {
+        const newState = this.state;
+        newState[chamber] = response.data.results;
+        this.setState(newState);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
